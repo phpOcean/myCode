7f0f4647286d367db26b09999a4dcb7e752c5e9a
phpOcean <51411106+phpOcean@users.noreply.github.com>
2022-10-13 16:01:38 +0800

    Add files via upload

--- multiplicationTable.php
+++ multiplicationTable.php
@@ -1,9 +1,13 @@
 <?php
-echo '<table border="0" align="center" width="800" >';
+echo '<table border="1" align="center" width="800" >';
 for ($i = 1; $i <= 9; $i++){
     echo '<tr>';
     for ($j = 1; $j <= $i; $j++){
         echo '<td>'.$j.'×'.$i.'='.($i*$j).'</td>';
+    }
+    //输出空白格
+    for ($nbsp = 9; $nbsp > $i; $nbsp--){
+        echo '<td>&nbsp;</td>';
     }
     echo '</tr>';
 }
